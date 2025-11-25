49769aff361ab4f685163b3796602402d45ba99b
Alexis <alexischristien.smp@gmail.com>
2025-11-25 18:55:34 +0100

    feat: add model paths to ValueConfig for better configuration management

--- project/src/config.py
+++ project/src/config.py
@@ -1,2 +1,5 @@
 class ValueConfig:
     TRAIN_METRICS = True
+    VECTORIZER_PATH = "project/models/tfidf_vectorizer.pkl"
+    SVM_PATH = "project/models/svm_model.pkl"
+    MODEL_PATH = "project/models/model.pkl"
