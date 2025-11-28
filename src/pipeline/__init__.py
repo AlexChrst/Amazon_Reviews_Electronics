e4a7cf759f3599aa47c318476a79abff70734d88
Alexis <alexischristien.smp@gmail.com>
2025-11-28 10:34:18 +0100

    feat: add __init__.py files for packaging

---
+++ src/pipeline/__init__.py
@@ -0,0 +1,6 @@
+from .vectorization_prediction import vectorize_texts, train_and_predict
+
+__all__ = [
+    "vectorize_texts",
+    "train_and_predict"
+]
---
+++ src/preprocessing/__init__.py
@@ -0,0 +1,8 @@
+from .clean_data import clean_dataset
+from .data_import_export import import_data, export_data
+
+__all__ = [
+    "clean_dataset",
+    "import_data",
+    "export_data"
+]
