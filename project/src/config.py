e915db2d96779730721fbdb56c2c8b1ab56b1289
Alexis <alexischristien.smp@gmail.com>
2025-11-28 10:34:58 +0100

    feat : delete the files inside project folder

--- project/src/config.py
+++
@@ -1,5 +0,0 @@
-class ValueConfig:
-    TRAIN_METRICS = True
-    VECTORIZER_PATH = "project/models/tfidf_vectorizer.pkl"
-    SVM_PATH = "project/models/svm_model.pkl"
-    MODEL_PATH = "project/models/model.pkl"
